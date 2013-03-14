69ead8e568a598ce50664914ec233f978d512035
Andrea Zonca <code@andreazonca.com>
2013-03-14 13:40:42 -0700

    numpy include

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+from numpy import get_include
 
-ext_modules = [Extension("accumulate", ["accumulate.pyx"], extra_compile_args=["-O3",]),
-               Extension("signalremove", ["signalremove.pyx"], extra_compile_args=["-O3",]),
+numpy_inc = get_include()
+
+ext_modules = [
+Extension("accumulate",   ["accumulate.pyx"],   include_dirs=[numpy_inc], extra_compile_args=["-O3",]),
+Extension("signalremove", ["signalremove.pyx"], include_dirs=[numpy_inc], extra_compile_args=["-O3",]),
 ]
 
 setup(
